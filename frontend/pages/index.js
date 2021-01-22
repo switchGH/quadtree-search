083264a615381375cbcb18101680620dd3031d3c
Tanoue Ryo <shg18.sk1.hr10@gmail.com>
2021-01-22 15:23:44 +0900

    add area handler.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import GoogleMap from '../components/GoogleMap'
 import Marker from '../components/Maker'
 import { getPlacesByLatlng } from '../api/api'
+import { getPath } from '../util/area_handler'
 
 const Home = () => {
   const [center, setCenter] = useState({ lat: 34.4111, lng: 135.3008 })
@@ -15,6 +16,9 @@ const Home = () => {
       ne_lat: 34.41583173345069, // 北東の緯度
       ne_lng: 135.30649265335796 // 北東の経度
     }
+    // pathを求める
+    const path = getPath(center);
+    console.log(path)
     // 範囲内の地点データを取得する
     const res_json = await getPlacesByLatlng(framaPoint).catch(e => {
       console.log(e)
@@ -39,7 +43,14 @@ const Home = () => {
 
   // ドラッグアンドドロップ後のアクション
   const onDragEnd = async (map) => {
-    console.log('Drag')
+    const center = {
+      lat: map.center.lat(),
+      lng: map.center.lng()
+    }
+    console.log(center)
+    // pathを求める
+    const path = getPath(center)
+    console.log(path)
     // 表示領域4点を取得する
     const latlngBounds = map.getBounds(); //表示領域の座標
     const swLatlng = latlngBounds.getSouthWest(); // 南西
